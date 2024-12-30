12058707b14c616ac92a1ba4237813d896f056a5
chaitanyakumar99 <gchkumar999@gmail.com>
2024-12-30 18:49:30 +0530

    Tt

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    otpToken: String,
+    otpTokenExpiry: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model("User", userSchema);
+
