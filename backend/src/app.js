097fd58495bd90bcdc0a727429555243c0d6fe8d
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-04 13:15:06 +0530

    txt

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express()
 
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const userRouter = require('./router/userRouter');
 app.use('/api/v1', userRouter)
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Testing</h1>')
